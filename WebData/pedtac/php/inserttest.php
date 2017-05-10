a05d5106fcdacfd490d386ed9f6801ad99e62aeb
Xiao,Jun-Hong <a4278910@gmail.com>
2017-05-10 17:13:39 +0800

    目錄錯誤

--- WebData/pedtac/php/inserttest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a["name"]="e";
-$a[0]=40;
-if(empty($a["name"])){
-    echo "Yes";
-}else{
-    echo "NO";
-}
-?>
--- motion2/AppDelegate.swift
+++ motion2/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  motion
+//  motion2
 //
-//  Created by David on 2017/2/4.
-//  Copyright © 2017年 DavidAppCoda. All rights reserved.
+//  Created by 蕭俊鴻 on 2017/2/22.
+//  Copyright © 2017年 蕭俊鴻. All rights reserved.
 //
 
 import UIKit
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -39,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        
     }
 
 
