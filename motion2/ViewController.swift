1b9f60f0035bae539dd0627615de6e1fefd9d6b2
Xiao,Jun-Hong <a4278910@gmail.com>
2017-05-11 14:23:30 +0800

    修改細節

--- motion2/ViewController.swift
+++ motion2/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     let mm = CMMotionManager()
     let formatter = DateFormatter()
-    var mydata = [Int:Any]()
+    var mydata = [String:Any]()
     var action:Int=0;
     
     override func viewDidLoad() {
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         var aw:Double=0
         var count:Int=0
         
+        
         formatter.dateFormat = "yyyy-M-d H:m:s"
         
         // Do any additional setup after loading the view, typically from a nib.
@@ -34,18 +35,31 @@ class ViewController: UIViewController {
                 let currentDate=NSDate()
                 let acc = motion.userAcceleration
                 let gyro = motion.rotationRate
+                let att = motion.attitude
+                var attYaw:CGFloat=0
+                var attRoll:CGFloat=0
+                var attPitch:CGFloat=0
                 let d=self.formatter.string(from: currentDate as Date)
                 var mylist=[String:Any]()
                 
+                attYaw=CGFloat(-att.yaw * 2 / M_PI) * 90
+                attRoll=CGFloat(-att.roll * 2 / M_PI) * 90
+                attPitch=CGFloat(-att.pitch * 2 / M_PI) * 90
                 af=abs(sqrt(pow(acc.x,2)+pow(acc.y,2)+pow(acc.z,2)))
                 aw=abs(sqrt(pow(gyro.x,2)+pow(gyro.y,2)+pow(gyro.z,2)))
-                print("af:\(af)\naw:\(aw)\ntime:\(d)")
                 mylist["Time"]=d
+                mylist["attYaw"]=attYaw
+                mylist["attRoll"]=attRoll
+                mylist["attPitch"]=attPitch
                 mylist["af"]=af
                 mylist["aw"]=aw
                 mylist["action"]=self.action
-                self.mydata[count]=mylist
+                self.mydata[String(count)]=mylist
                 count += 1
+                if(self.mydata.count>=100){
+                    count=0
+                    self.PostData()
+                }
             }
         }
     }
@@ -59,6 +73,25 @@ class ViewController: UIViewController {
                 btn.setTitle("靜止", for: .normal)
             }
         }
+    }
+    
+    func PostData(){
+        let urlstr="http://120.119.80.94:9487/"
+        let url = URL(string: urlstr)
+        let jsonData = try? JSONSerialization.data(withJSONObject: mydata)
+        var request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+        //request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody=jsonData
+        request.httpMethod = "POST"
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let dataTask = session.dataTask(with: request){(data, response, error) in
+            if let data = data{
+                self.mydata=[:]
+            }
+        }
+        dataTask.resume()
     }
     
     override func didReceiveMemoryWarning() {
